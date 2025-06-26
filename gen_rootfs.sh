e22a351343593f67fa70755267fbd15a66de3e1d
Wig Cheng <wigcheng@ieiworld.com>
2025-06-26 17:40:04 +0800

    gen_rootfs: copy default wallpaper for qemu-weston environment

--- gen_rootfs.sh
+++ gen_rootfs.sh
@@ -40,6 +40,7 @@ gen_pure_rootfs() {
     sudo cp -a ${TOP}/libs_overlay/vpu_libs/imx8mp/kernel-6.6.23/imx-vpuwrap/ ${TOP}/rootfs/
     sudo cp -a ${TOP}/libs_overlay/vpu_libs/imx8mp/kernel-6.6.23/kernel_headers/ ${TOP}/rootfs/
     sync
+    sudo cp -a ${TOP}/libs_overlay/wallpapers/wallpaper.png ${TOP}/rootfs/etc/
     QEMU_FILE="qemu_install-weston.sh"
   else
     QEMU_FILE="qemu_install.sh"
